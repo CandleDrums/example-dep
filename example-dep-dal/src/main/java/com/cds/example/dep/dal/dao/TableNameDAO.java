c09740722ea3f671363e60944d6a983ddc813603
liming <liming@limingdembp>
2020-08-07 10:59:00 +0800

    修改

---
+++ example-dep-dal/src/main/java/com/cds/example/dep/dal/dao/TableNameDAO.java
@@ -0,0 +1,22 @@
+/**
+ * @Project example-dep-dal
+ * @package com.cds.example.dep.dal.dao
+ * @Class TableNameDAO.java
+ * @Date [date]
+ * @Copyright [copyright]
+ */
+package com.cds.example.dep.dal.dao;
+
+import com.cds.base.dal.dao.MyBatisBaseDAO;
+import com.cds.example.dep.dal.model.TableNameDO;
+import com.cds.example.dep.dal.model.TableNameDOExample;
+
+/**
+ * @Description [name]DAO
+ * @Notes 未填写备注
+ * @author [author]
+ * @Date [date]
+ */
+public interface TableNameDAO extends MyBatisBaseDAO<TableNameDO, Integer, TableNameDOExample> {
+
+}
---
+++ example-dep-dal/src/main/java/com/cds/example/dep/dal/model/TableNameDO.java
@@ -0,0 +1,27 @@
+/**
+ * @Project example-dep-dal
+ * @package com.cds.example.dep.dal.model
+ * @Class TableNameDO.java
+ * @Date [date]
+ * @Copyright [copyright]
+ */
+package com.cds.example.dep.dal.model;
+
+import com.cds.base.dal.model.GeneralModel;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Description [name]DO
+ * @Notes 未填写备注
+ * @author [author]
+ * @Date [date]
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class TableNameDO extends GeneralModel {
+
+    private static final long serialVersionUID = 1L;
+
+}
